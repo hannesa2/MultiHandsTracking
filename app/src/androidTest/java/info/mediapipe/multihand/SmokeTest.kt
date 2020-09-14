faa4e228e207901e1ade8800cd82ec45883f7e01
Hannes Achleitner <hannes.software@gmx.at>
2020-09-14 11:21:09 +0200

    CI tests

---
+++ app/src/androidTest/java/info/mediapipe/multihand/SmokeTest.kt
@@ -0,0 +1,32 @@
+package info.mediapipe.multihand
+
+import android.Manifest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import com.moka.utils.Screenshot
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SmokeTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    @Test
+    fun smokeTestSimplyStart() {
+        onView(withId(R.id.preview_display_layout)).check(matches(isDisplayed()))
+        Screenshot.takeScreenshot("smoke")
+    }
+}
