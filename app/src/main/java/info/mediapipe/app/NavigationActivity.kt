552cbd26b615307153a543fa7f89ae3b9037236d
Hannes Achleitner <hannes.software@gmx.at>
2020-12-31 12:14:37 +0100

    Prepare for more use cases

---
+++ app/src/main/java/info/mediapipe/app/NavigationActivity.kt
@@ -0,0 +1,64 @@
+package info.mediapipe.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import info.mediapipe.app.multihand.MultiHandActivity
+
+abstract class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val headerLayout = navigationView.getHeaderView(0)
+        val textVersion = headerLayout.findViewById<TextView>(R.id.textVersion)
+        textVersion.text = BuildConfig.VERSION
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+        if (id == R.id.nav_multihand) {
+            openActivity(MultiHandActivity::class.java)
+        }
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    protected fun openActivity(clazz: Class<*>) {
+        startActivity(Intent(this, clazz))
+        if (clazz.isInstance(NavigationActivity::class.java)) {
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        supportInvalidateOptionsMenu()
+    }
+}
