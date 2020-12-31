006247946d2963a79b1b1ca35dcbec83b6318926
Hannes Achleitner <hannes.software@gmx.at>
2020-12-31 08:01:43 +0100

    Cleanup

--- app/src/main/java/info/mediapipe/multihand/app/HandApplication.kt
+++ app/src/main/java/info/mediapipe/multihand/app/HandApplication.kt
@@ -1,13 +1,11 @@
 package info.mediapipe.multihand.app
 
-import android.annotation.SuppressLint
 import android.app.Application
 import info.hannes.timber.DebugTree
 import timber.log.Timber
 
 class HandApplication : Application() {
 
-    @SuppressLint("HardwareIds")
     override fun onCreate() {
         super.onCreate()
 
