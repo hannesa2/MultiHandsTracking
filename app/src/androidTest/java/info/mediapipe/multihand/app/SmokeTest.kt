cb03133737bb0519a4ba7c8c65221b743cc1912d
Hannes Achleitner <hannes.software@gmx.at>
2021-02-27 18:50:49 +0100

    Espresso smoke test

--- app/src/androidTest/java/info/mediapipe/multihand/app/SmokeTest.kt
+++ app/src/androidTest/java/info/mediapipe/multihand/app/SmokeTest.kt
@@ -1,34 +1,51 @@
 package info.mediapipe.multihand.app
 
 import android.Manifest
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import android.util.Log
+import androidx.test.core.app.takeScreenshot
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import com.moka.utils.Screenshot
-import info.mediapipe.app.R
 import info.mediapipe.app.multihand.MultiHandActivity
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SmokeTest {
 
     @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(MultiHandActivity::class.java)
+    val activityScenarioRule = activityScenarioRule<MultiHandActivity>()
+
+    // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
+    @get:Rule
+    var nameRule = TestName()
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     @Test
     fun smokeTestSimplyStart() {
-        onView(withId(R.id.preview_display_layout)).check(matches(isDisplayed()))
-        Screenshot.takeScreenshot("smoke")
+        for (i in 3000L..5000L step 1000) {
+            Thread.sleep(i)
+            takeScreenshot()
+                .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-S$i")
+            try {
+                Espresso.onView(ViewMatchers.isRoot())
+                    .captureToBitmap()
+                    .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-A$i")
+            } catch (e: Exception) {
+                Log.e("smokeTestSimplyStart", e.message.toString())
+            }
+        }
     }
 }
