6eca39ba68332f665891e2838ff62c95b671dd59
Hannes Achleitner <hannes.software@gmx.at>
2026-02-15 17:49:17 +0100

    Remove Moka

--- app/src/androidTest/java/info/mediapipe/multihand/app/SmokeTest.kt
+++ app/src/androidTest/java/info/mediapipe/multihand/app/SmokeTest.kt
@@ -1,14 +1,17 @@
 package info.mediapipe.multihand.app
 
 import android.Manifest
+import android.graphics.Bitmap
+import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.captureToBitmap
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import com.moka.utils.Screenshot
 import info.mediapipe.app.R
 import info.mediapipe.app.multihand.MultiHandActivity
 import org.junit.Rule
@@ -29,6 +32,8 @@ class SmokeTest {
     @Test
     fun smokeTestSimplyStart() {
         onView(withId(R.id.preview_display_layout)).check(matches(isDisplayed()))
-        Screenshot.takeScreenshot("smoke")
+        onView(ViewMatchers.isRoot())
+            .perform(captureToBitmap { bitmap: Bitmap -> bitmap.writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}") })
+
     }
 }
